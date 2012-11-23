668bdcb4b932f3f77c00d708c91f7176bbccc702
Robert Einsle <robert@einsle.de>
2012-11-23 20:44:47 +0100

    Removed unused import
    Change-Id: Ie2d4fd47a68793373289dc1f8faabee60951d23a
    Signed-off-by: Robert Einsle <robert@einsle.de>

--- src/main/java/org/libreoffice/ci/gerrit/buildbot/servlets/LogfileServlet.java
+++ src/main/java/org/libreoffice/ci/gerrit/buildbot/servlets/LogfileServlet.java
@@ -3,7 +3,6 @@ package org.libreoffice.ci.gerrit.buildbot.servlets;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
